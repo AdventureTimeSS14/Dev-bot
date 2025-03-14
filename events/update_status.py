b82ff744ec198ad2fffa6f61bda93981a1611896
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-03-14 15:16:52 +0300

    pylint events\update_status.py

--- events/update_status.py
+++ events/update_status.py
@@ -1,11 +1,9 @@
-import time
 from datetime import datetime, timezone
 from urllib.parse import urlparse, urlunparse
 
 import aiohttp
 import dateutil.parser
 import disnake
-import requests
 
 # Определение уровней игры для SS14
 SS14_RUN_LEVELS = {0: "Лобби", 1: "Раунд идёт", 2: "Окончание раунда..."}
