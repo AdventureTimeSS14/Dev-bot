7c30fd888a5614808fae6220982a2197a4b6ad48
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-03-14 15:34:29 +0300

    Fix PyLinter components\button_help_components.py

--- components/button_help_components.py
+++ components/button_help_components.py
@@ -2,10 +2,29 @@ from disnake import ButtonStyle
 from disnake.ui import ActionRow, Button
 
 # Создание кнопок
-button_help = Button(label="&help", style=ButtonStyle.green, custom_id="button_help_id")
-button_team_help = Button(label="&team_help", style=ButtonStyle.blurple, custom_id="button_team_help_id")
-button_git_help = Button(label="&git_help", style=ButtonStyle.gray, custom_id="button_git_help_id")
-button_admin_help = Button(label="&admin_help", style=ButtonStyle.red, custom_id="button_admin_help_id")
+button_help = Button(
+    label="&help",
+    style=ButtonStyle.green,
+    custom_id="button_help_id"
+)
+
+button_team_help = Button(
+    label="&team_help",
+    style=ButtonStyle.blurple,
+    custom_id="button_team_help_id"
+)
+
+button_git_help = Button(
+    label="&git_help",
+    style=ButtonStyle.gray,
+    custom_id="button_git_help_id"
+)
+
+button_admin_help = Button(
+    label="&admin_help",
+    style=ButtonStyle.red,
+    custom_id="button_admin_help_id"
+)
 
 button_bug_report = Button(
     label="🔧 Оставить отзыв / Сообщить о баге",
@@ -17,7 +36,7 @@ action_row_button_help = ActionRow(
     button_help,
     button_team_help,
     button_git_help,
-    button_admin_help
+    button_admin_help,
 )
 
 action_row_bug_report = ActionRow(
