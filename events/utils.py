8e7d94a07db69cb6404d4a20484ca366860444ad
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-03-14 15:14:58 +0300

    pylint events\utils.py

--- events/utils.py
+++ events/utils.py
@@ -70,20 +70,32 @@ async def get_github_link(repo_code, number):
                 reviewed_reviewers_logins = [review['user']['login'] for review in reviews_data]
 
                 # Объединяем список назначенных и тех, кто уже поставил отзыв (не повторяем)
-                all_reviewers = set(requested_reviewers_logins + reviewed_reviewers_logins)  # Теперь это список логинов
+                # Теперь это список логинов
+                all_reviewers = set(requested_reviewers_logins + reviewed_reviewers_logins)
                 all_reviewers_str = ', '.join(all_reviewers) if all_reviewers else "Нет ревьюеров"
 
                 # Добавляем информацию о всех ревьюерах
-                embed.add_field(name="Ревьюеры 🔍", value=all_reviewers_str, inline=True)
+                embed.add_field(
+                    name="Ревьюеры 🔍",
+                    value=all_reviewers_str,
+                    inline=True
+                )
             else:
-                embed.add_field(name="Ревьюеры 🔍", value=", ".join(requested_reviewers_logins), inline=True)
+                embed.add_field(name="Ревьюеры 🔍",
+                                value=", ".join(requested_reviewers_logins),
+                                inline=True
+                )
 
             # Информация о том, кто одобрил (approved) PR
             reviews_url = f"{pr_url}/reviews"
             reviews_response = GLOBAL_SESSION.get(reviews_url)
             if reviews_response.status_code == 200:
                 reviews_data = reviews_response.json()
-                approved_reviewers = [review['user']['login'] for review in reviews_data if review['state'] == 'APPROVED']
+                approved_reviewers = (
+                    [review['user']['login']
+                    for review in reviews_data 
+                    if review['state'] == 'APPROVED']
+                )
                 if approved_reviewers:
                     approved_reviewers_str = ', '.join(approved_reviewers)
                     embed.add_field(name="Одобрение 🌟", value=approved_reviewers_str, inline=True)
@@ -92,7 +104,11 @@ async def get_github_link(repo_code, number):
 
             # Метки (Labels)
             labels = pr_data.get('labels', [])
-            labels_str = ', '.join([f"[{label['name']}]" for label in labels]) if labels else "Нет меток 🏷️"
+            labels_str = (
+                ', '.join([f"[{label['name']}]" for label in labels])
+                if labels
+                else "Нет меток 🏷️"
+            )
             embed.add_field(name="Метки 🏷️", value=labels_str, inline=True)
 
             # Количество комментариев
@@ -107,7 +123,11 @@ async def get_github_link(repo_code, number):
                 added_lines = sum(file['additions'] for file in diffstat_data)
                 deleted_lines = sum(file['deletions'] for file in diffstat_data)
                 # Добавляем изменения строк в нужном формате
-                embed.add_field(name="Изменение строк 🔄", value=f"Добавлено: +{added_lines}\nУдалено: −{deleted_lines}", inline=True)
+                embed.add_field(
+                    name="Изменение строк 🔄",
+                    value=f"Добавлено: +{added_lines}\nУдалено: −{deleted_lines}",
+                    inline=True
+                )
 
 
             # Форматируем даты в более читаемый вид
@@ -117,20 +137,28 @@ async def get_github_link(repo_code, number):
             closed_at_str = ""
             if closed_at:
                 closed_at = datetime.strptime(closed_at, "%Y-%m-%dT%H:%M:%SZ")
-                closed_at_str = closed_at.strftime("• Закрыт {0}".format(closed_at.strftime("%d %b %Y, %H:%M")))
+                closed_at_str = (
+                    f"• Закрыт {closed_at.strftime('%d %b %Y, %H:%M')}"
+                )
 
             created_at_str = created_at.strftime("%d %b %Y, %H:%M")  # Например, 30 Jun 2024, 09:59
             updated_at_str = updated_at.strftime("%d %b %Y, %H:%M")  # Например, 30 Jun 2024, 12:16
 
             # Дополнительная информация о PR с улучшенным форматированием
-            embed.set_footer(text=f"PR открыт {created_at_str} • Обновлен {updated_at_str} {closed_at_str}")
+            embed.set_footer(
+                text=f"PR открыт {created_at_str} • Обновлен {updated_at_str} {closed_at_str}"
+            )
 
             # Завершающие теги
             if draft:
                 embed.set_footer(text=f"Этот PR в работе. #WIP ⚙️ • {updated_at_str}")
 
             # Ссылка на PR
-            embed.add_field(name="Ссылка на PR 🔗", value=f"[Перейти в PR]({pr_data['html_url']})", inline=False)
+            embed.add_field(
+                name="Ссылка на PR 🔗",
+                value=f"[Перейти в PR]({pr_data['html_url']})",
+                inline=False
+            )
 
             return embed
 
