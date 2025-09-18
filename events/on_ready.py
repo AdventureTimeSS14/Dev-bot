0704f28ea9043320123247622a6b3b8a7af736f0
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-09-18 13:12:11 +0300

    - Правки запуска таско

--- events/on_ready.py
+++ events/on_ready.py
@@ -82,8 +82,8 @@ async def on_ready():
             (update_permission_stats, "Update Permission Stats"),
             (clear_doker_replay_ss14, "Clear Doker Replay"),
             (check_size_log, "Check Adminlogs size"),
-            (update_admin_info_embed, "Update Admin Info Embed"),
-            (enforce_category_block, "Enforce Category Block")
+            (update_admin_info_embed, "Update Admin Info Embed")
+            # (enforce_category_block, "Enforce Category Block")
         ]
         # search_results = await search_bans_in_multiple_channels("TestSearcheeeeeeeeeeeeeee")
 
