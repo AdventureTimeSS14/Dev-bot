11a79a69939bd1ddffde26163ebde4edaca40f26
Darki255 <Kurachiy75@gmail.com>
2025-10-16 22:51:24 +0300

    Добавил права доступа к команде привязки

--- commands/discord/link_account_command.py
+++ commands/discord/link_account_command.py
@@ -1,7 +1,10 @@
 from bot_init import bot, ss14_db
+from dataConfig import ROLE_ACCESS_TOP_HEADS
+from disnake.ext.commands import has_any_role
 
 import uuid
 
+@has_any_role(*ROLE_ACCESS_TOP_HEADS)
 @bot.command(name="link_account")
 async def link_command(ctx, guid: str, ds_id: str):
     if await ss14_db.is_linked(ds_id):
